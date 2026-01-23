ef01061c077a8223523e0472d9d52ffd14ee6107
Sajincl-03 <sajincl222@gmail.com>
2026-01-23 14:44:25 +0530

    product schema created

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,5 +1,36 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    
-});
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  brand: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Brand',
+    required: true
+  },
+  stock: {
+    type: Number,
+    min: 0,
+    required: true
+  },
+  price: {
+    type: Number,
+    min: 0,
+    required: true
+  },
+  image_url: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
