4eeac0c2cfff661d6dd49072c8342cd866c1145b
Sajincl-03 <sajincl222@gmail.com>
2026-03-07 02:27:59 +0530

    chatbot addded for testing purpose

--- client/src/layouts/UserLayout.jsx
+++ client/src/layouts/UserLayout.jsx
@@ -3,11 +3,22 @@ import UserHeader from '@/components/UserHeader';
 
 
 function UserLayout() {
+
+  const userRole = "customer"; 
+  useEffect(() => {
+    if (userRole === "customer") {
+      const script = document.createElement("script");
+      script.src = "//code.tidio.co/cngwrmvpafnjqjkafhuq1wtzxk1y94um.js";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  }, [userRole]);
   return (
+
     <>
       <UserHeader />
       <main>
-        <Outlet /> 
+        <Outlet />
       </main>
     </>
   )
