94a6faa73a0da9421e329b464e7d6ba64ee4558f
sajin-cl <sajincl222@gmail.com>
2026-06-11 22:53:41 +0530

    removed:chatbot

--- client/src/layouts/UserLayout.jsx
+++ client/src/layouts/UserLayout.jsx
@@ -4,25 +4,7 @@ import { useEffect } from 'react';
 import { useAuth } from "@/hooks/useAuth";
 
 function UserLayout() {
-  const { role } = useAuth();
 
-  useEffect(() => {
-    if (!role) return;
-
-    if (role !== 'admin' && role !== 'seller') {
-
-      const script = document.createElement("script");
-      script.id = "tidio-script";
-      script.src = "//code.tidio.co/cngwrmvpafnjqjkafhuq1wtzxk1y94um.js";
-      script.async = true;
-      document.body.appendChild(script);
-    }
-    else {
-      const script = document.createElement("script");
-      if (script) script.remove();
-    }
-
-  }, [role]);
 
   return (
     <>
